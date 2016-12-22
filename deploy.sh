e81e24f03a99abbbd7c6874c23045d533bbfc530
Tieme Togola <tftogola@gmail.com>
2016-12-22 14:45:46 -0500

    do not run on dirty working copy

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,24 @@
+git diff-index --quiet HEAD --
+status=$?
+
+if [ $status -eq 1 ]; then
+    echo "ABORT: working directory is dirty"
+    exit 1
+fi
+
 echo "polymer build..."
 polymer build &&
 
 echo "copying to docs/..."
 cp -rf build/bundled/ docs/ &&
 
-echo "create CNAME file..."
+echo "create docs/CNAME file..."
 echo "komoscope.com" > docs/CNAME &&
 
-git stash
 git add docs/ &&
 git commit -m "github pages deploy build" && # TODO: build number?
 
 echo "publish to github pages..."
-git push origin master
+git push &&
 
-git stash apply
-
-#TODO: print deploy status messages to stdout
+exit 0
